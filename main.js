0c5e2caafdccf0f73064d2899ec2d3b0e3ed9ba4
Denis Novikov (dehimer) <den-vse@yandex.ru>
2016-09-29 18:56:32 +0300

    permanent period of interval

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ var settingsWindow = null;
 
 const mainWindowSizes = [335, 300];
 
+
 app.on('ready', function() {
     console.log('ready');
 
@@ -37,22 +38,49 @@ ipc.on('main-window:pressing-power', function (event, power) {
     pressingPower = power;
 });
 
+
+var SERVER_HOST;
+
+/* UDP CLIENT FOR SERVER LIGHTING */
+var PORT = 5567;
+var dgram = require('dgram');
+var client = dgram.createSocket('udp4');
+
+client.on('listening', function () {
+    var address = client.address();
+    console.log('UDP Client listening on ' + address.address + ":" + address.port);
+    client.setBroadcast(true);
+});
+
+client.on('message', function (message, rinfo) {
+    SERVER_HOST = rinfo.address;
+    console.log('Message from: ' + rinfo.address + ':' + rinfo.port +' - ' + message);
+
+});
+
+
+client.bind(PORT);
+
 console.log(process.versions);
 ipc.on('main-window:set-setup', function (event, data) {
     console.log(data);
-    if(mainWindow && data && data.id && data.port){
+    if(mainWindow && data && data.id){
 
         mainWindow.webContents.send('main-window:ready-indicate');
 
 
-        /* UDP CLIENT*/
+        /* UDP CLIENT FOR COLORS */
         var ID = data.id;
-        
-        var HOST = data.ip || '127.0.0.1';
-        var PORT = data.port;
+
+        var SERVER_PORT = 3000;
 
         var dgram = require('dgram');
         var val = 0;
+
+
+        var curTs;
+        var delay = 100;
+
 
         function sendState() {
 
@@ -69,16 +97,20 @@ ipc.on('main-window:set-setup', function (event, data) {
             }
 
             var message = new Buffer(ID+' '+valueToSend);
+            curTs = +(new Date());
+            if(SERVER_HOST){
+                var client = dgram.createSocket('udp4');
+                client.send(message, 0, message.length, SERVER_PORT, SERVER_HOST, function(err, bytes) {
+                    if (err) throw err;
+                    // console.log(message)
+                    console.log('UDP message sent to ' + SERVER_PORT +':'+ SERVER_HOST);
+                    client.close();
 
-            var client = dgram.createSocket('udp4');
-            client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
-                if (err) throw err;
-                // console.log(message)
-                // console.log('UDP message sent to ' + HOST +':'+ PORT);
-                client.close();
-
-                setTimeout(sendState, 100);
-            });    
+                    setTimeout(sendState, delay - (+(new Date())-curTs) );
+                });    
+            }else{
+                setTimeout(sendState, delay - (+(new Date())-curTs));
+            }
         }
 
         sendState();
@@ -86,55 +118,27 @@ ipc.on('main-window:set-setup', function (event, data) {
 
 
         /* E1.31 SERVER */
-        // new Buffer([0x41, 0x53, 0x43, 0x2d, 0x45, 0x31, 0x2e, 0x31, 0x37, 0x00, 0x00, 0x00]);
         var e131 = require('e131');
         var dgram = require('dgram');
-        // dgram.createSocket('udp4');
-        // dgram.createSocket('udp4');
 
         
-        // UNIVERSE #1
-        var universe_big = new e131.Server(ID*2-1);
+        // UNIVERSES
+        var universes = new e131.Server([1, 2]);//
         // console.log(ID*2-1);
-        universe_big.on('listening', function() {
-            console.log('universe_big listening on port %d, universe %d', this.port, this.universe);
+        universes.on('listening', function() {
+            console.log(this.universes);
+            console.log('universes listening on port %d, universes %s', this.port, this.universes.join(','));
         });
-        universe_big.on('packet', function (packet) {
+        universes.on('packet', function (packet) {
           
             var sourceName = packet.getSourceName();
             var sequenceNumber = packet.getSequenceNumber();
             var universe = packet.getUniverse();
             var channelData = packet.getChannelData();
-            
             var hexcolor = '#'+channelData.toString('hex', 0, 3)
-            mainWindow.webContents.send('main-window:big-circle-color', hexcolor);
+            mainWindow && mainWindow.webContents.send('main-window:big-circle-color', hexcolor);
 
-            console.log(hexcolor);
-
-            /*
-            console.log('source="%s", seq=%d, universe=%d, channels=%d',
-                sourceName, sequenceNumber, universe, channelData.length);
-            console.log('channel data = %s', channelData.toString('hex'));
-            */
-        });
-
-
-        // UNIVERSE #2
-        var universe_small = new e131.Server(ID*2);
-        // console.log(ID*2);
-        universe_small.on('listening', function() {
-            console.log('universe_small listening on port %d, universe %d', this.port, this.universe);
-        });
-        universe_small.on('packet', function (packet) {
-          
-            var sourceName = packet.getSourceName();
-            var sequenceNumber = packet.getSequenceNumber();
-            var universe = packet.getUniverse();
-            var channelData = packet.getChannelData();
-
-            // console.log();
-            var hexcolor = '#'+channelData.toString('hex', 0, 3)
-            mainWindow.webContents.send('main-window:small-circle-color', hexcolor);
+            console.log(universe+' : '+hexcolor);
             /*
             console.log('source="%s", seq=%d, universe=%d, channels=%d',
                 sourceName, sequenceNumber, universe, channelData.length);
@@ -145,5 +149,3 @@ ipc.on('main-window:set-setup', function (event, data) {
 
     }
 });
-
-
